a133655c5c36b5975391c19cfeafb6bdee09485d
egert <egertons@163.com>
2021-02-17 21:28:46 +0800

    2021_2_17_21_28

---
+++ src/main/java/com/zhanglin/dao/BookMapper.java
@@ -0,0 +1,19 @@
+package com.zhanglin.dao;
+
+import com.zhanglin.pojo.Books;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BookMapper {
+    //增加一本书
+    public int addBook(Books books);
+    //删除一本书
+    public int deleteBookById(@Param("id") int id);
+    //更新一本书
+    public int updateBook(Books books);
+    //查询一本书
+    public Books queryById(@Param("id") int id);
+    //查询全部的书
+    public List<Books> queryAllBooks();
+}
---
+++ src/main/java/com/zhanglin/service/BookService.java
@@ -0,0 +1,18 @@
+package com.zhanglin.service;
+
+import com.zhanglin.pojo.Books;
+
+import java.util.List;
+
+public interface BookService {
+    //增加一本书
+    public int addBook(Books books);
+    //删除一本书
+    public int deleteBookById(int id);
+    //更新一本书
+    public int updateBook(Books books);
+    //查询一本书
+    public Books queryById(int id);
+    //查询全部的书
+    public List<Books> queryAllBooks();
+}
