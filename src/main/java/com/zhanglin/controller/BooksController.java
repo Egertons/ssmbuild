94ea960eafcfdfb505d60192bea26a627e36933b
egert <egertons@163.com>
2021-02-19 00:47:57 +0800

    2021_2_19_04_47

---
+++ src/main/java/com/zhanglin/controller/BooksController.java
@@ -0,0 +1,28 @@
+package com.zhanglin.controller;
+
+import com.zhanglin.pojo.Books;
+import com.zhanglin.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class BooksController {
+
+    @Autowired
+    @Qualifier("BookServiceImpl")
+    private BookService bookService;
+
+    @RequestMapping("/all")
+    public String getAllBook(Model model){
+        List<Books> books = bookService.queryAllBooks();
+        model.addAttribute("list",books);
+        return "allbook";
+    }
+
+}
